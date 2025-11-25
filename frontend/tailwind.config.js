35c8fa83793f43d8e1d9fcd6eb47e7a8ed9caf77
Wassim brs <wassim.bouras@outlook.com>
2025-11-25 11:00:49 +0100

    frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx}", // On cible les fichiers JS et JSX
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
